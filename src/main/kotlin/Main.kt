416745ba5f93350d2cd75299bf8571e5fd8ec4c3
Izotov Valentin <devices111@yandex.ru>
2026-09-19 17:39:03 +0300

    Спринт 7. Приложение Заметки

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import service.ArchiveMenu
+
+fun main() {
+    println("Добро пожаловать в приложение 'Заметки'!")
+    ArchiveMenu().show()
+    println("Программа завершена. До свидания!")
 }
